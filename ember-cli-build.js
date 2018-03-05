73ae6d3cc45d8cd49ed80408a563ea772ae0ab10
Robert Wagner <rwwagner90@gmail.com>
2018-03-04 20:48:25 -0500

    Add most deps

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    brotli: {
+      enabled: false,
+      appendSuffix: false
+    },
     'ember-bootstrap': {
       'bootstrapVersion': 3,
       'importBootstrapFont': true,
@@ -13,6 +17,16 @@ module.exports = function(defaults) {
       'components': ['scss', 'javascript', 'handlebars', 'http', 'json', 'bash'],
       'plugins': ['normalize-whitespace', 'show-language']
     },
+    'ember-service-worker': {
+      enabled: false
+    },
+    treeShaking: {
+      enabled: false,
+      include: [
+        'ember-validators/format.js',
+        'ember-validators/presence.js'
+      ]
+    }
   });
 
   return app.toTree();
